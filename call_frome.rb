6d45a6f005ae4d9b9277ed5ffa6121212c310b50
Kisitu Augustine <austiine04@gmail.com>
2014-07-19 21:00:50 +0300

    refactor - extracts callframe class to stand alone file

---
+++ call_frome.rb
@@ -0,0 +1,69 @@
+module Micro
+  class CallFrame
+
+    def initialize(state, function, local = [], depth=0)
+      @state = state
+      @code = function.code
+      @literals = function.literals
+      @locals = locals
+      @depth = depth
+      @stack = [] 
+    end
+
+    def execute
+      @code.each |instruction| do 
+        case instruction.opcode 
+        when :push_literal 
+          @stack.push @literals[instruction.opcode] 
+        when :push_local 
+          @stack.push @literals[instruction.opcode] 
+        when :pop 
+          @stack.pop 
+        when :add 
+          operand_one = @stack.pop 
+          operand_two = @stack.pop 
+          check_type(Fixnum, operand_one, operand_two) 
+          @stack.push operand_one + operand_two
+        when :sub
+          operand_one = @stack.pop 
+          operand_two = @stack.pop 
+          check_type(Fixnum, operand_one, operand_two) 
+          @stack.push operand_one - operand_two 
+        when :mul 
+          operand_one = @stack.pop 
+          operand_two = @stack.pop 
+          check_type(Fixnum, operand_one, operand_two) 
+          @stack.push operand_one * operand_two 
+        when :div 
+          operand_one = @stack.pop 
+          operand_two = @stack.pop 
+          check_type(Fixnum, operand_one, operand_two) raise ZeroDivisionError if operand_two == 0 
+          @stack.push operand_one / operand_two 
+        when :call 
+          argsnum = instruction.operand 
+          function_name = @stack.pop 
+          function = @state.find_function(function_name) 
+          locals = [] argsnum.times { locals.push @stack.pop } 
+          if !function raise Namerror, 
+            "function #{function_name} doesnot exist" 
+          end 
+
+          if @depth > MAX_STACK_DEPTH 
+            raise "Call stack level too deep" 
+          end 
+          call_frame = CallFrame.new(@state, function, locals, @depth + 1)
+        when :ret
+          @stack.pop
+        end
+      end
+      raise "Broken bytecode: lack of RET code"
+    end
+
+    def check_type(type, *operands)
+      operands.each do |operand|
+        raise TypeError, "#{operand.inspect} is not a #{type}" unless operand.is_a?(type)
+      end
+    end
+
+  end
+end 

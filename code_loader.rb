74b6eddb21939be145bca176e9ba0b35aaf041c2
Kisitu Augustine <austiine04@gmail.com>
2014-07-19 21:01:29 +0300

    refactor - extracts code_loader class to stand alone file

---
+++ code_loader.rb
@@ -0,0 +1,42 @@
+module Micro
+  class CodeLoader
+    OPCODES = [:push_literal, :push_local, :pop, :add, :sub, :mul, :div, :call, :ret ]
+
+    def self.load_file(filename)
+      functions = []
+      file = File.open(filename)
+
+      while !file.eof? && line = file.readline
+        if line =~/^_/
+          load_function_on_stack line
+        elsif line =~/^:/
+          load_function_arguments_on_stack line
+        else
+          opcode, operand = line.split('.').map(&:to_i) 
+          functions.last.code.push VM::Instruction.new(OPCODES[opcode], operand)
+        end
+      end
+
+      functions
+    end
+  end
+
+  private
+
+  def load_function_on_stack function
+    functions.push VM::Function.new(function.chomp[1..-2].to_sym)
+    functions.last.code = []
+    functions.last.literals = []
+  end
+
+  def load_function_arguments_on_stack arguments 
+    functions.last.literals = arguments[1..-1].split(',').map do |argument|
+      if literal = /\"/
+        argument.chomp[1..-2]
+      else
+        argument.to_i
+      end
+    end
+  end
+
+end

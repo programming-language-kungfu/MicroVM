6c8e231babc67f6c084c9191b594ccabfd2e06ef
austiine04 <austiine04@gmail.com>
2016-01-19 15:36:07 +1100

    constant is from vm class

--- call_frame.rb
+++ call_frame.rb
@@ -1,3 +1,5 @@
+require './micro_vm'
+
 module Micro
   class CallFrame
     def initialize(state, function, locals = [], depth=0)
@@ -6,55 +8,55 @@ module Micro
       @literals = function.operands
       @locals = locals
       @depth = depth
-      @stack = [] 
+      @stack = []
     end
 
     def execute
-      @code.each do |instruction| 
-        case instruction.opcode 
-        when :push_literal 
-          @stack.push @literals[instruction.opcode] 
-        when :push_local 
-          @stack.push @literals[instruction.opcode] 
-        when :pop 
-          @stack.pop 
-        when :add 
-          operand_one = @stack.pop 
-          operand_two = @stack.pop 
-          check_type(Fixnum, operand_one, operand_two) 
+      @code.each do |instruction|
+        case instruction.opcode
+        when :push_literal
+          @stack.push @literals[instruction.opcode]
+        when :push_local
+          @stack.push @literals[instruction.opcode]
+        when :pop
+          @stack.pop
+        when :add
+          operand_one = @stack.pop
+          operand_two = @stack.pop
+          check_type(Fixnum, operand_one, operand_two)
           @stack.push operand_one + operand_two
         when :sub
-          operand_one = @stack.pop 
-          operand_two = @stack.pop 
-          check_type(Fixnum, operand_one, operand_two) 
-          @stack.push operand_one - operand_two 
-        when :mul 
-          operand_one = @stack.pop 
-          operand_two = @stack.pop 
-          check_type(Fixnum, operand_one, operand_two) 
-          @stack.push operand_one * operand_two 
-        when :div 
-          operand_one = @stack.pop 
-          operand_two = @stack.pop 
-          check_type(Fixnum, operand_one, operand_two) 
-          raise ZeroDivisionError, "cant divide by zero" if operand_two == 0 
-          @stack.push operand_one / operand_two 
-        when :call 
-          argsnum = instruction.operand 
-          function_name = @stack.pop 
-          function = @state.find_function(function_name) 
-          locals = [] 
-          argsnum.times { locals.push @stack.pop } 
-          
-          if !function 
-            raise Namerror, "function #{function_name} doesnot exist" 
-          end 
+          operand_one = @stack.pop
+          operand_two = @stack.pop
+          check_type(Fixnum, operand_one, operand_two)
+          @stack.push operand_one - operand_two
+        when :mul
+          operand_one = @stack.pop
+          operand_two = @stack.pop
+          check_type(Fixnum, operand_one, operand_two)
+          @stack.push operand_one * operand_two
+        when :div
+          operand_one = @stack.pop
+          operand_two = @stack.pop
+          check_type(Fixnum, operand_one, operand_two)
+          raise ZeroDivisionError, "cant divide by zero" if operand_two == 0
+          @stack.push operand_one / operand_two
+        when :call
+          argsnum = instruction.operand
+          function_name = @stack.pop
+          function = @state.find_function(function_name)
+          locals = []
+          argsnum.times { locals.push @stack.pop }
 
-          if @depth > MAX_STACK_DEPTH 
-            raise "Call stack level too deep" 
-          end 
+          if !function
+            raise Namerror, "function #{function_name} doesnot exist"
+          end
 
-          call_frame = CallFrame.new(@state, function, locals, @depth + 1)
+          if @depth > ::VM::MAX_STACK_DEPTH
+            raise "Call stack level too deep"
+          end
+
+          CallFrame.new(@state, function, locals, @depth + 1)
         when :ret
           @stack.pop
         end
@@ -69,4 +71,4 @@ module Micro
     end
 
   end
-end 
+end
